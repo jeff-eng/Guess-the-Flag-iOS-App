81fea73b81fb7c140710fc08d4a3a20565ba09d0
Jeff Eng <jeff@jeffeng.me>
2016-07-05 16:16:41 -0700

    created layout with buttons, added flag assets, and created outlets to the ViewController

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Jeffrey Eng on 7/5/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var button1: UIButton!
+
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
